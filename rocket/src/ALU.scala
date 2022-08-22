00d25a865c2ae6c20003feae677e27c9bd4ebef5
Jiuyang Liu <liu@jiuyang.me>
2022-08-22 12:46:38 +0000

    update ALU to use new Operands object and refactor to chisel3.

--- rocket/src/ALU.scala
+++ rocket/src/ALU.scala
@@ -3,96 +3,61 @@
 
 package org.chipsalliance.rocket
 
-import Chisel._
-import freechips.rocketchip.config.Parameters
-import freechips.rocketchip.tile.CoreModule
+import chisel3._
+import chisel3.util.{Cat, Fill, Reverse}
+import org.chipsalliance.rocket.Operands._
+import org.chipsalliance.rocket.Operands.ALU.helper._
 
-object ALU
-{
-  val SZ_ALU_FN = 4
-  def FN_X    = BitPat("b????")
-  def FN_ADD  = UInt(0)
-  def FN_SL   = UInt(1)
-  def FN_SEQ  = UInt(2)
-  def FN_SNE  = UInt(3)
-  def FN_XOR  = UInt(4)
-  def FN_SR   = UInt(5)
-  def FN_OR   = UInt(6)
-  def FN_AND  = UInt(7)
-  def FN_SUB  = UInt(10)
-  def FN_SRA  = UInt(11)
-  def FN_SLT  = UInt(12)
-  def FN_SGE  = UInt(13)
-  def FN_SLTU = UInt(14)
-  def FN_SGEU = UInt(15)
-
-  def FN_DIV  = FN_XOR
-  def FN_DIVU = FN_SR
-  def FN_REM  = FN_OR
-  def FN_REMU = FN_AND
-
-  def FN_MUL    = FN_ADD
-  def FN_MULH   = FN_SL
-  def FN_MULHSU = FN_SEQ
-  def FN_MULHU  = FN_SNE
-
-  def isMulFN(fn: UInt, cmp: UInt) = fn(1,0) === cmp(1,0)
-  def isSub(cmd: UInt) = cmd(3)
-  def isCmp(cmd: UInt) = cmd >= FN_SLT
-  def cmpUnsigned(cmd: UInt) = cmd(1)
-  def cmpInverted(cmd: UInt) = cmd(0)
-  def cmpEq(cmd: UInt) = !cmd(3)
+class ALUIO(xLen: Int) extends Bundle {
+  val dw = Input(UInt(DW.width.W))
+  val fn = Input(UInt(ALU.width.W))
+  val in2 = Input(UInt(xLen.W))
+  val in1 = Input(UInt(xLen.W))
+  val out = Output(UInt(xLen.W))
+  val adder_out = Output(UInt(xLen.W))
+  val cmp_out = Output(Bool())
 }
-
-import ALU._
-
-class ALU(implicit p: Parameters) extends CoreModule()(p) {
-  val io = new Bundle {
-    val dw = Bits(INPUT, SZ_DW)
-    val fn = Bits(INPUT, SZ_ALU_FN)
-    val in2 = UInt(INPUT, xLen)
-    val in1 = UInt(INPUT, xLen)
-    val out = UInt(OUTPUT, xLen)
-    val adder_out = UInt(OUTPUT, xLen)
-    val cmp_out = Bool(OUTPUT)
-  }
+class ALU(xLen: Int) extends Module {
+  val io: ALUIO = IO(new ALUIO(xLen))
 
   // ADD, SUB
-  val in2_inv = Mux(isSub(io.fn), ~io.in2, io.in2)
-  val in1_xor_in2 = io.in1 ^ in2_inv
+  val in2_inv: UInt = Mux(isSub(io.fn), ~io.in2, io.in2)
+  val in1_xor_in2: UInt = io.in1 ^ in2_inv
   io.adder_out := io.in1 + in2_inv + isSub(io.fn)
 
   // SLT, SLTU
   val slt =
-    Mux(io.in1(xLen-1) === io.in2(xLen-1), io.adder_out(xLen-1),
-    Mux(cmpUnsigned(io.fn), io.in2(xLen-1), io.in1(xLen-1)))
-  io.cmp_out := cmpInverted(io.fn) ^ Mux(cmpEq(io.fn), in1_xor_in2 === UInt(0), slt)
+    Mux(io.in1(xLen - 1) === io.in2(xLen - 1), io.adder_out(xLen - 1),
+      Mux(cmpUnsigned(io.fn), io.in2(xLen - 1), io.in1(xLen - 1)))
+  io.cmp_out := cmpInverted(io.fn) ^ Mux(cmpEq(io.fn), in1_xor_in2 === 0.U, slt)
 
   // SLL, SRL, SRA
   val (shamt, shin_r) =
-    if (xLen == 32) (io.in2(4,0), io.in1)
+    if (xLen == 32) (io.in2(4, 0), io.in1)
     else {
       require(xLen == 64)
       val shin_hi_32 = Fill(32, isSub(io.fn) && io.in1(31))
-      val shin_hi = Mux(io.dw === DW_64, io.in1(63,32), shin_hi_32)
-      val shamt = Cat(io.in2(5) & (io.dw === DW_64), io.in2(4,0))
-      (shamt, Cat(shin_hi, io.in1(31,0)))
+      val shin_hi = Mux(io.dw === DW.Y, io.in1(63, 32), shin_hi_32)
+      val shamt = Cat(io.in2(5) & (io.dw === DW.Y), io.in2(4, 0))
+      (shamt, Cat(shin_hi, io.in1(31, 0)))
     }
-  val shin = Mux(io.fn === FN_SR  || io.fn === FN_SRA, shin_r, Reverse(shin_r))
-  val shout_r = (Cat(isSub(io.fn) & shin(xLen-1), shin).asSInt >> shamt)(xLen-1,0)
+  val shin = Mux(io.fn === ALU.SR || io.fn === ALU.SRA, shin_r, Reverse(shin_r))
+  val shout_r = (Cat(isSub(io.fn) & shin(xLen - 1), shin).asSInt >> shamt) (xLen - 1, 0)
   val shout_l = Reverse(shout_r)
-  val shout = Mux(io.fn === FN_SR || io.fn === FN_SRA, shout_r, UInt(0)) |
-              Mux(io.fn === FN_SL,                     shout_l, UInt(0))
+  val shout = Mux(io.fn === ALU.SR || io.fn === ALU.SRA, shout_r, 0.U) |
+    Mux(io.fn === ALU.SL, shout_l, 0.U)
 
   // AND, OR, XOR
-  val logic = Mux(io.fn === FN_XOR || io.fn === FN_OR, in1_xor_in2, UInt(0)) |
-              Mux(io.fn === FN_OR || io.fn === FN_AND, io.in1 & io.in2, UInt(0))
+  val logic = Mux(io.fn === ALU.XOR || io.fn === ALU.OR, in1_xor_in2, 0.U) |
+    Mux(io.fn === ALU.OR || io.fn === ALU.AND, io.in1 & io.in2, 0.U)
   val shift_logic = (isCmp(io.fn) && slt) | logic | shout
-  val out = Mux(io.fn === FN_ADD || io.fn === FN_SUB, io.adder_out, shift_logic)
+  val out = Mux(io.fn === ALU.ADD || io.fn === ALU.SUB, io.adder_out, shift_logic)
 
   io.out := out
   if (xLen > 32) {
     require(xLen == 64)
-    when (io.dw === DW_32) { io.out := Cat(Fill(32, out(31)), out(31,0)) }
+    when(io.dw === DW.N) {
+      io.out := Cat(Fill(32, out(31)), out(31, 0))
+    }
   }
 }
