0c93e851f1d70e3e3f390d76289599e238d20804
Jiuyang Liu <liu@jiuyang.me>
2022-09-27 09:08:19 +0800

    prepare AMOALU operands for global reference

--- rocket/src/Operands.scala
+++ rocket/src/Operands.scala
@@ -1,5 +1,6 @@
 package org.chipsalliance.rocket
 
+import chisel3._
 import chisel3.util.BitPat
 
 /** micro-ops for rocket. */
@@ -9,5 +10,22 @@ object Operands {
   def Y: BitPat = BitPat.Y()
 
   def N: BitPat = BitPat.N()
+  object MEM {
+    val width: Int = 5
+    val X: BitPat = BitPat.dontCare(width)
+
+    /** atomic ALU uop. */
+    object XA {
+      val SWAP = "b00100".U(width.W)
+      val ADD = "b01000".U(width.W)
+      val XOR = "b01001".U(width.W)
+      val OR = "b01010".U(width.W)
+      val AND = "b01011".U(width.W)
+      val MIN = "b01100".U(width.W)
+      val MAX = "b01101".U(width.W)
+      val MINU = "b01110".U(width.W)
+      val MAXU = "b01111".U(width.W)
+    }
+  }
 }
 
